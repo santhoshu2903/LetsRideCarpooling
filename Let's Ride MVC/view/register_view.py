5d1afc318bd56d1f047cadfa3b092e653e29d767
Santhosh Vara Prasad Uppuluri <santhosh18412@gmail.com>
2023-09-22 12:35:06 -0400

    MVC Updated register view

--- Let's Ride MVC/view/register_view.py
+++ Let's Ride MVC/view/register_view.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 class RegisterView(tk.Tk):
     def __init__(self, controller):
@@ -13,10 +13,6 @@ class RegisterView(tk.Tk):
         style.configure("TButton", font=("Segoe UI", 10, "bold"), borderwidth=0)
         style.configure("TLabel", font=("Segoe UI", 12), background="#f4f4f4")
         style.configure("TEntry", font=("Segoe UI", 10))
-        self.show_register()
-
-    def show_register(self):
-        self.clear_content()
 
         self.notebook = ttk.Notebook(self)
         
@@ -35,19 +31,19 @@ class RegisterView(tk.Tk):
         self.rider_password_entry = ttk.Entry(self.rider_frame, show="*")
         self.rider_password_entry.pack(pady=10)
 
-        phone_extensions = ["+1", "+44", "+91", "+33"]  # Example extensions, you can add more
-
-        self.rider_password_label = ttk.Label(self.rider_frame, text="Rider Phone Number:")
-        self.rider_password_label.pack(pady=10)
+        phone_extensions = ["+1", "+44", "+91", "+33"]
 
         self.rider_phone_extension_combobox = ttk.Combobox(self.rider_frame, values=phone_extensions)
         self.rider_phone_extension_combobox.pack(pady=10)
-        self.rider_phone_extension_combobox.set(phone_extensions[0])  # Set default value
+        self.rider_phone_extension_combobox.set(phone_extensions[0])
+
+        self.rider_phone_number_label = ttk.Label(self.rider_frame, text="Rider Phone Number:")
+        self.rider_phone_number_label.pack(pady=10)
 
         self.rider_phone_number_entry = ttk.Entry(self.rider_frame)
         self.rider_phone_number_entry.pack(pady=10)
 
-        self.rider_register_btn = ttk.Button(self.rider_frame, text="Register as Rider", command=self.register_user)  # You can customize the command for rider-specific registration
+        self.rider_register_btn = ttk.Button(self.rider_frame, text="Register as Rider", command=self.controller.register_rider)
         self.rider_register_btn.pack(pady=10)
 
         self.notebook.add(self.rider_frame, text="Rider")
@@ -67,17 +63,17 @@ class RegisterView(tk.Tk):
         self.passenger_password_entry = ttk.Entry(self.passenger_frame, show="*")
         self.passenger_password_entry.pack(pady=10)
 
-        self.passenger_password_label = ttk.Label(self.passenger_frame, text="Passenger Phone Number:")
-        self.passenger_password_label.pack(pady=10)
-        
         self.passenger_phone_extension_combobox = ttk.Combobox(self.passenger_frame, values=phone_extensions)
         self.passenger_phone_extension_combobox.pack(pady=10)
-        self.passenger_phone_extension_combobox.set(phone_extensions[0])  # Set default value
+        self.passenger_phone_extension_combobox.set(phone_extensions[0])
+
+        self.passenger_phone_number_label = ttk.Label(self.passenger_frame, text="Passenger Phone Number:")
+        self.passenger_phone_number_label.pack(pady=10)
 
         self.passenger_phone_number_entry = ttk.Entry(self.passenger_frame)
         self.passenger_phone_number_entry.pack(pady=10)
 
-        self.passenger_register_btn = ttk.Button(self.passenger_frame, text="Register as Passenger", command=self.register_user)  # You can customize the command for passenger-specific registration
+        self.passenger_register_btn = ttk.Button(self.passenger_frame, text="Register as Passenger", command=self.controller.register_passenger)
         self.passenger_register_btn.pack(pady=10)
 
         self.notebook.add(self.passenger_frame, text="Passenger")
@@ -90,3 +86,7 @@ class RegisterView(tk.Tk):
     def clear_content(self):
         for widget in self.winfo_children():
             widget.destroy()
+
+if __name__ == "__main__":
+    app = RegisterView(None)
+    app.mainloop()
