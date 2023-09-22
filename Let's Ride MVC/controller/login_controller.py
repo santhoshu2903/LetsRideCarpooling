a7ec8cca9509c3e0a7c6990613119d76546a9e8a
Santhosh Vara Prasad Uppuluri <santhosh18412@gmail.com>
2023-09-22 12:37:28 -0400

    MVC Controllers Updated

--- Let's Ride MVC/controller/login_controller.py
+++ Let's Ride MVC/controller/login_controller.py
@@ -1,15 +1,43 @@
-from view import login_view
-from model import db
-import sqlite3
-import random
 import tkinter as tk
-from tkinter import ttk, messagebox
-from twilio.rest import Client
+import random
+from tkinter import messagebox
+from view.login_view import LoginView
+from view.welcome_view import WelcomeView
 
 class LoginController:
-    def __init__(self):
-        self.view = login_view.LoginView(self)
+    def __init__(self, root):
+        self.root = root
+        self.view = LoginView(self)
+        self.view.mainloop()
 
-    def show_login(self):
+    def send_otp(self):
+        # Implement sending OTP logic here (e.g., using Twilio)
+        # Generate OTP (for example, a random 6-digit number)
+        otp_generated = str(random.randint(100000, 999999))
+        # Send the OTP to the user's phone number
+
+        # For demonstration purposes, we'll show a messagebox with the generated OTP
+        messagebox.showinfo("OTP Sent", f"OTP sent to your phone: {otp_generated}")
+
+    def login_user(self):
+        phone_extension = self.view.phone_extension_combobox.get()
+        phone_number = self.view.phone_number_entry.get()
+        otp_entered = self.view.otp_entry.get()
+
+        if not phone_number or not otp_entered:
+            messagebox.showerror("Error", "Both fields are required!")
+            return
+
+        # Check OTP validity (you can implement this logic)
+
+        # For demonstration purposes, we'll show a messagebox with a successful login message
+        messagebox.showinfo("Success", "Login successful!")
+
+    def show_welcome(self):
         self.view.clear_content()
-        self.view.show_login()
+        welcome_controller = WelcomeController(self.root)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = LoginController(root)
+    root.mainloop()
---
+++ Let's Ride MVC/controller/welcome_controller.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from view.welcome_view import WelcomeView
+from view.login_view import LoginView
+from view.register_view import RegisterView
+
+class WelcomeController:
+    def __init__(self, root):
+        self.root = root
+        self.view = WelcomeView(self)
+        self.view.mainloop()
+
+    def show_login(self):
+        self.view.clear_content()
+        login_controller = LoginController(self.root)
+
+    def show_register(self):
+        self.view.clear_content()
+        register_controller = RegisterController(self.root)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = WelcomeController(root)
+    root.mainloop()
