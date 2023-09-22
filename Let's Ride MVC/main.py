f9e6817cd858b4f32f2c42b78a128b0720c012e1
Santhosh Vara Prasad Uppuluri <santhosh18412@gmail.com>
2023-09-22 15:58:55 -0400

    Remove Entire MVC Structure, fresh start again

--- Let's Ride MVC/main.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from controller.welcome_controller import WelcomeController
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = WelcomeController(root)
-    root.mainloop()
