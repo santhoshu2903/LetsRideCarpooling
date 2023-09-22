af9a32799aeff05ef1ad07b3d81d43859e225a79
Santhosh Vara Prasad Uppuluri <santhosh18412@gmail.com>
2023-09-22 11:25:09 -0400

    code corrupted and logic collapsed, re writing all again

--- Let's Ride MVC/model/db.py
+++ Let's Ride MVC/model/db.py
@@ -1,38 +1,53 @@
 import sqlite3
+import os
 
-RIDERS_DB = "riders.db"
-PASSENGERS_DB = "passengers.db"
+DATABASE_DIR = os.path.join(os.getcwd(), "Database")
+RIDERS_DB = os.path.join(DATABASE_DIR, "riders.db")
+PASSENGERS_DB = os.path.join(DATABASE_DIR, "passengers.db")
+ALL_USERS_DB = os.path.join(DATABASE_DIR, "all_users.db")
 
-def init_dbs():
-    init_db(RIDERS_DB)
-    init_db(PASSENGERS_DB)
-
-def init_db(db_name):
-    with sqlite3.connect(db_name) as conn:
+def init_db():
+    # Initialize the riders database
+    with sqlite3.connect(RIDERS_DB) as conn:
         cursor = conn.cursor()
         cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            username TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL,
-            role TEXT DEFAULT ''
-        );
-        ''')
+        CREATE TABLE IF NOT EXISTS riders (
+        userid INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        phone_number TEXT,
+        phone_extension TEXT
+    );
+    ''')
 
+    # Initialize the passengers database
+    with sqlite3.connect(PASSENGERS_DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS passengers (
+        userid INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        phone_number TEXT,
+        phone_extension TEXT
+    );
+    ''')
 
-def add_user_to_db(db_name, username, password):
+    # Initialize the all users database( contains userid, phone number, and name)
+    with sqlite3.connect(ALL_USERS_DB) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS all_users (
+        userid INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        phone_number TEXT,
+        phone_extension TEXT
+    );
+    ''')
+
+def add_user_to_db(db_name, username, password, phone_extension, phone_number):
     with sqlite3.connect(db_name) as conn:
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        cursor.execute("INSERT INTO users (username, password, phone_extension, phone_number) VALUES (?, ?, ?, ?)",
+                       (username, password, phone_extension, phone_number))
         conn.commit()
-
-def add_rider(username, password):
-    add_user_to_db(RIDERS_DB, username, password)
-
-def add_passenger(username, password):
-    add_user_to_db(PASSENGERS_DB, username, password)
-
-def get_user(db_name, username):
-    with sqlite3.connect(db_name) as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE username=?", (username,))
-        return cursor.fetchone()
